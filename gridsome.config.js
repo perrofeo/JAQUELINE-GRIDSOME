30fc59743f31a804d462872acce39bc6192110d7
igor <perrofeofilms@gmail.com>
2019-08-12 18:55:55 -0600

    url

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteUrl: `https://gifted-golick-fa1831.netlify.com/`,
   host: "0.0.0.0",
   titleTemplate: "%s - NWA",
   siteDescription: "Creative technologist",
